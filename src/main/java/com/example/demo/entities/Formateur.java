6a795c4b7edcb7c51819fa69e2aafd51b930ffb3
imaneELMAZZOUZY <imane.imm0210@gmail.com>
2023-04-26 12:27:55 +0100

    first commit

--- src/main/java/com/example/demo/entities/Formateur.java
+++ src/main/java/com/example/demo/entities/Formateur.java
@@ -14,9 +14,17 @@ import java.util.List;
 @NoArgsConstructor
 @DiscriminatorValue("FORM")
 public class Formateur extends Utilisateur{
+
+
     @ManyToMany(mappedBy = "formateurs")
     private List<Competence> competences = new ArrayList<Competence>();
 
     @OneToMany(mappedBy = "formateur")
     private List<Validation> validations = new ArrayList<Validation>();
+    public Formateur(String nom,String prenom,String password,String email,String telephone,List<Competence> comp,List<Validation> val)
+    {
+        super(null,nom,prenom,password,email,telephone);
+        competences=comp;
+        validations=val;
+    }
 }
